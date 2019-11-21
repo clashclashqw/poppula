8e659627982ed32019e9af870c02b837374e83ab
clashclashqw <57956988+clashclashqw@users.noreply.github.com>
2019-11-22 01:01:29 +0700

    Reformatting code

--- Week2/2/main.go
+++ Week2/2/main.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-number1 := 5
-number2 := 7
-number3 := (number1 + number2)
-fmt.Print(number3)
+  number1 := 5
+  number2 := 7
+  number3 := (number1 + number2)
+  
+  fmt.Print(number3)
 }
