4ce4ab2375bf7ce7de1e4694776e8aee9b1242eb
clashclashqw <57956988+clashclashqw@users.noreply.github.com>
2019-11-24 17:46:51 +0700

    Create main.go

---
+++ Week2/8/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println(3 + 2)
+  fmt.Println(3 - 2.199)
+  fmt.Println(3.0 / 2.0)
+  fmt.Println(3 % 2)
+  fmt.Println(78 - 5.492)
+  fmt.Println(42 * 96)
+  fmt.Println(1025 - 492)
+}
