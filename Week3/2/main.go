1b8631c7524e0d73da28a79b03b4d7802f540b3f
clashclashqw <57956988+clashclashqw@users.noreply.github.com>
2019-11-28 23:42:43 +0700

    but Print Achievement

--- Week3/2/main.go
+++ Week3/2/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	n, e := fmt.Print("Hello", "World", 123, 456, "\n")
-	fmt.Println("number of bytes written :", n, "\n")
-	fmt.Println("write erro encountered :", e, "\n")
+	fmt.Print("number of bytes written :", n, "\n")
+	fmt.Print("write erro encountered :", e, "\n")
 
 }
