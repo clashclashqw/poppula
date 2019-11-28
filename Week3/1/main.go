0468ad9d65c21a6040d66c560ed715fa209a6297
clashclashqw <57956988+clashclashqw@users.noreply.github.com>
2019-11-28 23:12:37 +0700

    Yaaaaaa! Good

--- Week3/1/main.go
+++ Week3/1/main.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("My name is %s, I am %d year old\n", "Goku", 18)
+	fmt.Printf("My name is %s, I am %d year old\n", "Goku", 18)
 
-	fmt.Println("type = %T \n", 3.14159265359)
-	fmt.Println("pi = %f \n", 3.14159265359)
-	fmt.Println("pi = %2.2f \n", 3.14159265359)
-	fmt.Println("pi = %9.f \n", 3.14159265359)
-	fmt.Println("pi = %-9.f \n", 3.14159265359)
-	fmt.Println("pi = %09.f \n", 3.14159265359)
-	fmt.Println("pi = %09.2f \n", 3.14159265359)
-	fmt.Println("pi = %E \n", 3.14159265359)
+	fmt.Printf("type = %T \n", 3.14159265359)
+	fmt.Printf("pi = %f \n", 3.14159265359)
+	fmt.Printf("pi = %2.2f \n", 3.14159265359)
+	fmt.Printf("pi = %9.f \n", 3.14159265359)
+	fmt.Printf("pi = %-9.f \n", 3.14159265359)
+	fmt.Printf("pi = %09.f \n", 3.14159265359)
+	fmt.Printf("pi = %09.2f \n", 3.14159265359)
+	fmt.Printf("pi = %E \n", 3.14159265359)
 
-	fmt.Println("1 > 2 = %t \n", 1 > 2)
-	fmt.Println("10 (base 2) = %b \n", 10)
-	fmt.Println("10 (base 8) = %o \n", 10)
-	fmt.Println("10 (base 10) = %d \n", 10)
-	fmt.Println("10 (base 16) = %x \n", 10)
+	fmt.Printf("1 > 2 = %t \n", 1 > 2)
+	fmt.Printf("10 (base 2) = %b \n", 10)
+	fmt.Printf("10 (base 8) = %o \n", 10)
+	fmt.Printf("10 (base 10) = %d \n", 10)
+	fmt.Printf("10 (base 16) = %x \n", 10)
 }
